a05b9fe074f4035b20cfec581cce1889f485499c
Oleg Kireev <okireev@ubic.tech>
2023-12-14 11:55:52 +0400

    Install vite-tsconfig-paths and use it

--- host/vite.config.ts
+++ host/vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import federation from '@originjs/vite-plugin-federation'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import federation from '@originjs/vite-plugin-federation';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,13 +12,14 @@ export default defineConfig({
       remotes: {
         remoteApp: 'http://localhost:5001/assets/remoteEntry.js',
       },
-      shared: ['react','react-dom']
-    })
+      shared: ['react', 'react-dom'],
+    }),
+    tsconfigPaths(),
   ],
   build: {
     modulePreload: false,
     target: 'esnext',
     minify: false,
-    cssCodeSplit: false
-  }
-})
+    cssCodeSplit: false,
+  },
+});
--- remote/vite.config.ts
+++ remote/vite.config.ts
@@ -1,24 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import federation from '@originjs/vite-plugin-federation'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     federation({
-      name: "remote_app",
-      filename: "remoteEntry.js",
+      name: 'remote_app',
+      filename: 'remoteEntry.js',
       exposes: {
-        './Button': './src/components/Button'
+        './Button': './src/components/Button',
       },
-      shared: ['react','react-dom']
-    })
+      shared: ['react', 'react-dom'],
+    }),
+    tsconfigPaths(),
   ],
   build: {
     modulePreload: false,
     target: 'esnext',
     minify: false,
-    cssCodeSplit: false
-  }
-})
+    cssCodeSplit: false,
+  },
+});
