f704f0df49741cce3191d77ad912bfc1e9c46f75
Oleg Kireev <okireev@ubic.tech>
2023-12-14 13:28:14 +0400

    Use FSD file structure

---
+++ host/src/app/index.tsx
@@ -0,0 +1,12 @@
+import { AppRouter } from './router';
+import './theme/index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
