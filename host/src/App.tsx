b35c82e8ae58f0e66fdeafe91909601f857bc2dc
Oleg Kireev <okireev@ubic.tech>
2023-12-14 12:45:26 +0400

    Import Homepage in host app

--- host/src/App.tsx
+++ host/src/App.tsx
@@ -1,5 +1,15 @@
+import { lazy, Suspense } from 'react';
+const HomePage = lazy(() => import('homepage/App'));
+
 function App() {
-  return <div className="App">Host App</div>;
+  return (
+    <div className="App">
+      <h1>Host App</h1>
+      <Suspense fallback="Loading homepage...">
+        <HomePage />
+      </Suspense>
+    </div>
+  );
 }
 
 export default App;
